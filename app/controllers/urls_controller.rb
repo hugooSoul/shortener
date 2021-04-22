40304ab6550132d9278697c73cbd4dbdad089b1b
Hugo Lopez <hugol.soul@gmail.com>
2021-04-22 17:05:27 -0500

    rendering created Urls

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,9 +1,9 @@
 class UrlsController < ApplicationController
-  validates_presence_of :long_url
-  validates :long_url, format: URI::regexp(%w[http https])
-  validates_uniqueness_of :short_url
-  validates_length_of :long_url, within: 3..255, on: :create, message: "too long"
-  validates_length_of :short_url, within: 3..255, on: :create, message: "too long"
+  #validates_presence_of :long_url
+  #validates :long_url, format: URI::regexp(%w[http https])
+  #validates_uniqueness_of :short_url
+  #validates_length_of :long_url, within: 3..255, on: :create, message: "too long"
+  #validates_length_of :short_url, within: 3..255, on: :create, message: "too long"
 
 
   def index
@@ -16,8 +16,8 @@ class UrlsController < ApplicationController
 
   def create
     @url = Url.new(url_params)
-    @url.short_url =@url.generate_short_url
-    @url.long_url = @url.sanitize
+    @url.short_url = generate_short_url
+    @url.long_url = sanitize
     if @url.save
       redirect_to urls_path
     else
@@ -31,19 +31,19 @@ class UrlsController < ApplicationController
     redirect_to @url.sanitize
   end
 
+  private
+
+  def url_params
+    params.require(:url).permit(:name, :long_url, :user_id, :category_id)
+  end
+
   def generate_short_url
     rand(36**8).to_s(36)
   end
 
   def sanitize
-    long_url.strip!
-    sanitize_url = self.long_url.downcase.gsub(/(https?:\/\/)|(www\.)/,"")
+    @url.long_url.strip!
+    sanitize_url = @url.long_url.downcase.gsub(/(https?:\/\/)|(www\.)/,"")
     "http://#{sanitize_url}"
-  end
-
-  private
-
-  def url_params
-    params.require(:url).permit(:name, :long_url, :user_id, :category_id)
   end
 end
