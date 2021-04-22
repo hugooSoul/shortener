48c982fbb51906fb668dfaa3b75865afaf8cb941
Hugo Lopez <hugol.soul@gmail.com>
2021-04-22 16:15:18 -0500

    Devise used for Users and implementeing logic for generate the URL slug

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,49 @@
+class UrlsController < ApplicationController
+  validates_presence_of :long_url
+  validates :long_url, format: URI::regexp(%w[http https])
+  validates_uniqueness_of :short_url
+  validates_length_of :long_url, within: 3..255, on: :create, message: "too long"
+  validates_length_of :short_url, within: 3..255, on: :create, message: "too long"
+
+
+  def index
+    @urls = Url.all
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(url_params)
+    @url.short_url =@url.generate_short_url
+    @url.long_url = @url.sanitize
+    if @url.save
+      redirect_to urls_path
+    else
+      flash[:error] = @url.errors.full_messages
+      redirect_to new_url_path
+    end
+  end
+
+  def show
+    @url = Url.find_by(short_url: params[:short_url])
+    redirect_to @url.sanitize
+  end
+
+  def generate_short_url
+    rand(36**8).to_s(36)
+  end
+
+  def sanitize
+    long_url.strip!
+    sanitize_url = self.long_url.downcase.gsub(/(https?:\/\/)|(www\.)/,"")
+    "http://#{sanitize_url}"
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:name, :long_url, :user_id, :category_id)
+  end
+end
