4355a9b7bedb76326b42801415e2c694be6c76e5
Hugo Lopez <hugol.soul@gmail.com>
2021-04-22 17:29:08 -0500

    adding sign in and out links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'urls#index'
 
   resources :urls, only: [:index, :new, :create, :show]
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
